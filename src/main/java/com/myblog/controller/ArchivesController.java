a8883e4edfde0c4b230ec72d3979bf7c136464c9
kungunil <909192324@qq.com>
2021-10-08 18:29:52 +0800

    这是项目框架

--- src/main/java/com/myblog/controller/ArchivesController.java
+++
@@ -1,9 +0,0 @@
-package com.myblog.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class ArchivesController {
-
-}
